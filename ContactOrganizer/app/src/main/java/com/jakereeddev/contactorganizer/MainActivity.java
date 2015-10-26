a65d540200f1252ff0b350a080430ae54d5cf064
jake32321 <jake32321@yahoo.com>
2015-10-26 00:00:37 -0500

    Added tab view for new content

--- ContactOrganizer/app/src/main/java/com/jakereeddev/contactorganizer/MainActivity.java
+++ ContactOrganizer/app/src/main/java/com/jakereeddev/contactorganizer/MainActivity.java
@@ -1,11 +1,15 @@
 package com.jakereeddev.contactorganizer;
 
+import android.inputmethodservice.KeyboardView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TabHost;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +26,27 @@ public class MainActivity extends AppCompatActivity {
         phoneTxt = (EditText) findViewById(R.id.contactPhoneNumber);
         emailTxt = (EditText) findViewById(R.id.contactEmail);
         addressTxt = (EditText) findViewById(R.id.contactAddress);
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
         addBtn = (Button) findViewById(R.id.addButton);
 
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("creator");
+        tabSpec.setContent(R.id.creatorTab);
+        tabSpec.setIndicator("Creator");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("list");
+        tabSpec.setContent(R.id.tabContactList);
+        tabSpec.setIndicator("Store");
+        tabHost.addTab(tabSpec);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Your contact has been created", Toast.LENGTH_SHORT).show();
+            }
+        });
         nameTxt.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
